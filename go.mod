c13d8865df39547bbd70601cb6d7e4006bfe57a1
Steve McCarthy <steve@redlua.com>
2025-06-28 16:08:53 -0400

    Update to use socketmode

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/ifo/slack-polls
 go 1.16
 
 require (
+	github.com/gorilla/websocket v1.5.3 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/slack-go/slack v0.8.2 // indirect
+	github.com/slack-go/slack v0.16.0 // indirect
 )
--- main.go
+++ main.go
@@ -11,12 +11,20 @@ import (
 	"strings"
 
 	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/socketmode"
 )
+
+type Config struct {
+	Client           *slack.Client
+	SigningSecret    string
+	SocketModeClient *socketmode.Client
+}
 
 func main() {
 	var (
 		port          string
 		apiKey        string
+		appToken      string
 		signingSecret string
 		isSet         bool
 	)
@@ -25,29 +33,65 @@ func main() {
 		port = "3000"
 	}
 
+	config := Config{}
+
 	if apiKey, isSet = os.LookupEnv("API_KEY"); !isSet {
 		log.Fatalln("No API_KEY set")
 	}
 
-	if signingSecret, isSet = os.LookupEnv("SIGNING_SECRET"); !isSet {
-		log.Fatalln("No SIGNING_SECRET set")
+	appToken = os.Getenv("APP_TOKEN")
+	signingSecret = os.Getenv("SIGNING_SECRET")
+
+	if appToken == "" && signingSecret == "" {
+		log.Fatalln("One of APP_TOKEN or SIGNING_SECRET was not set")
 	}
 
-	config := Config{
-		Client:        slack.New(apiKey),
-		SigningSecret: signingSecret,
+	if signingSecret != "" {
+		config.SigningSecret = signingSecret
+		config.Client = slack.New(apiKey)
+
+		http.HandleFunc("/slash", config.SlashHandler)
+		http.HandleFunc("/modal", config.ModalHandler)
+
+		log.Printf("starting server on port %s\n", port)
+		log.Fatalln(http.ListenAndServe(":"+port, nil))
+		return
 	}
 
-	http.HandleFunc("/slash", config.SlashHandler)
-	http.HandleFunc("/modal", config.ModalHandler)
+	config.Client = slack.New(apiKey, slack.OptionAppLevelToken(appToken))
+	config.SocketModeClient = socketmode.New(
+		config.Client,
+		// Options to enable for debugging:
+		//socketmode.OptionDebug(true),
+		//socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+	socketmodeHandler := socketmode.NewSocketmodeHandler(config.SocketModeClient)
 
-	log.Printf("starting server on port %s\n", port)
-	log.Fatalln(http.ListenAndServe(":"+port, nil))
+	socketmodeHandler.Handle(socketmode.EventTypeConnecting, middlewareConnecting)
+	socketmodeHandler.Handle(socketmode.EventTypeConnectionError, middlewareConnectionError)
+	socketmodeHandler.Handle(socketmode.EventTypeConnected, middlewareConnected)
+
+	socketmodeHandler.HandleSlashCommand("/poll", config.PollSocketHandler)
+	socketmodeHandler.HandleSlashCommand("/slash", config.SlashSocketHandler)
+	socketmodeHandler.Handle(socketmode.EventTypeInteractive, config.ModalSocketHandler)
+
+	log.Print("starting socket mode")
+	err := socketmodeHandler.RunEventLoop()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
-type Config struct {
-	Client        *slack.Client
-	SigningSecret string
+func middlewareConnecting(evt *socketmode.Event, client *socketmode.Client) {
+	log.Println("Connecting to Slack with Socket Mode...")
+}
+
+func middlewareConnectionError(evt *socketmode.Event, client *socketmode.Client) {
+	log.Println("Connection failed. Retrying later...")
+}
+
+func middlewareConnected(evt *socketmode.Event, client *socketmode.Client) {
+	log.Println("Connected to Slack with Socket Mode.")
 }
 
 func (c *Config) SlashHandler(w http.ResponseWriter, r *http.Request) {
@@ -78,6 +122,108 @@ func (c *Config) SlashHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func (c *Config) PollSocketHandler(evt *socketmode.Event, client *socketmode.Client) {
+	cmd, ok := evt.Data.(slack.SlashCommand)
+	if !ok {
+		log.Printf("Ignored event: %+v\n", evt)
+		return
+	}
+
+	client.Ack(*evt.Request)
+
+	client.Debugf("Slash command received: %+v", cmd)
+
+	modalRequest := generateModalRequest()
+	_, err := c.Client.OpenView(cmd.TriggerID, modalRequest)
+	if err != nil {
+		log.Printf("Error opening view: %v", err)
+	}
+}
+
+func (c *Config) SlashSocketHandler(evt *socketmode.Event, client *socketmode.Client) {
+	cmd, ok := evt.Data.(slack.SlashCommand)
+	if !ok {
+		log.Printf("Ignored event: %+v\n", evt)
+		return
+	}
+
+	client.Ack(*evt.Request)
+
+	client.Debugf("Slash command received: %+v", cmd)
+
+	modalRequest := generateModalRequest()
+	_, err := c.Client.OpenView(cmd.TriggerID, modalRequest)
+	if err != nil {
+		log.Printf("Error opening view: %v", err)
+	}
+}
+
+func (c *Config) ModalSocketHandler(evt *socketmode.Event, client *socketmode.Client) {
+	callback, ok := evt.Data.(slack.InteractionCallback)
+	if !ok {
+		log.Printf("Ignored %+v\n", evt)
+		return
+	}
+
+	client.Debugf("Interaction received: %+v\n", callback)
+
+	client.Ack(*evt.Request)
+
+	switch callback.Type {
+	case slack.InteractionTypeBlockActions:
+		blockSetIndex := 2
+		messageTimestamp := callback.Message.Timestamp
+		userToAdd := "<@" + callback.User.ID + ">"
+		channel := callback.Channel.ID
+		optionSelected, _ := strconv.Atoi(callback.ActionCallback.BlockActions[0].Value)
+		newMessageBlocks := callback.Message.Msg.Blocks
+
+		groupTexts := []string{}
+		for _, i := range []int{1, 3, 5, 7} {
+			groupTexts = append(groupTexts, newMessageBlocks.BlockSet[blockSetIndex].(*slack.SectionBlock).Fields[i].Text)
+		}
+		groupTexts = updateGroups(userToAdd, optionSelected-1, groupTexts)
+		for k, v := range map[int]int{1: 0, 3: 1, 5: 2, 7: 3} {
+			newMessageBlocks.BlockSet[blockSetIndex].(*slack.SectionBlock).Fields[k].Text = groupTexts[v]
+		}
+
+		if err := c.updateMessage(channel, messageTimestamp, slack.MsgOptionBlocks(newMessageBlocks.BlockSet...)); err != nil {
+			log.Printf("API update message error: %v", err)
+		}
+		return
+	case slack.InteractionTypeViewSubmission:
+		buttons := []*slack.ButtonBlockElement{}
+		textBlocks := []*slack.TextBlockObject{}
+		for _, numStr := range []string{"1", "2", "3", "4"} {
+			str := callback.View.State.Values["choice"+numStr]["choice"+numStr].Value
+			text := slack.NewTextBlockObject("plain_text", str, false, false)
+			textBlocks = append(textBlocks, text, slack.NewTextBlockObject("mrkdwn", ":", false, false)) // turns out mrkdwn is the key
+			button := slack.NewButtonBlockElement("actionID"+numStr, numStr, text)
+			buttons = append(buttons, button)
+		}
+		actionBlock := slack.NewActionBlock("", buttons[0], buttons[1], buttons[2], buttons[3])
+		sectionBlock := slack.SectionBlock{
+			Type:   slack.MBTSection,
+			Fields: textBlocks,
+		}
+		question := callback.View.State.Values["question"]["question"].Value
+		headerText := slack.NewTextBlockObject("plain_text", question, true, false)
+		headerBlock := slack.SectionBlock{
+			Type: slack.MBTSection,
+			Text: headerText,
+		}
+		channel := callback.View.State.Values["channel"]["channelActionID"].SelectedConversation
+		if channel == "" {
+			channel = callback.User.ID
+		}
+
+		if err := c.sendMessage(channel, slack.MsgOptionBlocks(headerBlock, actionBlock, sectionBlock)); err != nil {
+			return
+		}
+	default:
+	}
+}
+
 func generateModalRequest() slack.ModalViewRequest {
 	question := makeTextInputBlock("Name of Post", "It's time to poll!", "question", "question")
 	choice1 := makeTextInputBlock("Choice 1", "2 hours", "choice1", "choice1")
@@ -85,8 +231,8 @@ func generateModalRequest() slack.ModalViewRequest {
 	choice3 := makeTextInputBlock("Choice 3", "4 months", "choice3", "choice3")
 	choice4 := makeTextInputBlock("Choice 4", "5 years", "choice4", "choice4")
 	channelSelect := slack.NewOptionsSelectBlockElement(slack.OptTypeConversations, slack.NewTextBlockObject("plain_text", "channel to post in", false, false), "channelActionID")
-	channelSelect.InitialConversation = "" // This should be the main channel ID
-	channel := slack.NewInputBlock("channel", slack.NewTextBlockObject("plain_text", "channel to post in", false, false), channelSelect)
+	channelSelect.InitialConversation = os.Getenv("INITIAL_CHANNEL") // This should be the main channel ID
+	channel := slack.NewInputBlock("channel", slack.NewTextBlockObject("plain_text", "channel to post in", false, false), slack.NewTextBlockObject("plain_text", " ", false, false), channelSelect)
 
 	// Create a ModalViewRequest with a header and two inputs
 	titleText := slack.NewTextBlockObject("plain_text", "Fun fun poll time!", false, false)
@@ -115,9 +261,10 @@ func generateModalRequest() slack.ModalViewRequest {
 
 func makeTextInputBlock(title, placeholder, returnName, blockName string) *slack.InputBlock {
 	text := slack.NewTextBlockObject("plain_text", title, false, false)
+	emptyText := slack.NewTextBlockObject("plain_text", " ", false, false)
 	placeholderStr := slack.NewTextBlockObject("plain_text", placeholder, false, false)
 	textInputBlock := slack.NewPlainTextInputBlockElement(placeholderStr, returnName)
-	return slack.NewInputBlock(blockName, text, textInputBlock)
+	return slack.NewInputBlock(blockName, text, emptyText, textInputBlock)
 }
 
 // ModalHandler handles all action responses from slack
